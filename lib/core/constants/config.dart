383bde5aad4a2472b68f062aa91fa243a63787b5
Eng-AkramShaker <akramaboubakersh@gmail.com>
2024-09-25 00:34:13 +0300

    25/09

--- lib/core/constants/config.dart
+++
@@ -1,9 +0,0 @@
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-// ignore_for_file: non_constant_identifier_names
-
-class Config {
-  static String baseUrl = 'http://93.127.215.9:5000/api/v1/';
-
-  static String get_All_question = "question?limit=100000000000";
-}
